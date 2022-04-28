a3e00f045a238adf7a3ae05b56b4a674085b5b26
Evan Cunningham <ecunningham@sd38.bc.ca>
2022-04-28 12:38:23 -0700

    adding promo card content section and major refinement to the header image and site name area

--- web/sites/dev/settings.php
+++ web/sites/dev/settings.php
@@ -2,6 +2,9 @@
 
 $settings['config_sync_directory'] = '../config/sync/default';
 
+$settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
+
+
 if (file_exists($app_root . '/sites/default/settings.base.php')) {
   include $app_root . '/sites/default/settings.base.php';
 }
