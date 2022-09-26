f8010bc33379b4c8bf7276987a7473e684e9944d
Evan Cunningham <ecunningham@sd38.bc.ca>
2022-09-26 12:25:18 -0700

    disabling the feeds import functionality until all day events can be handled and imported correctly

--- web/sites/dev/settings.php
+++ web/sites/dev/settings.php
@@ -4,6 +4,7 @@ $settings['config_sync_directory'] = '../config/sync/default';
 
 $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
 
+$settings['file_private_path'] = $app_root . '/../private';
 
 if (file_exists($app_root . '/sites/default/settings.base.php')) {
   include $app_root . '/sites/default/settings.base.php';
